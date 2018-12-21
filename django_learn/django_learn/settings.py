cc6ac9d7c0a3bf53b0c34d0c9a83b2eba7570ef2
duanguiyuan <1092765650@qq.com>
2018-12-20 22:08:49 -0500

    先使用调试模式开发，非调试模式下静态文件连接不到，这个问题后期再解决

--- django_learn/django_learn/settings.py
+++ django_learn/django_learn/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '**$#$b&x#^0%a8$bu-3v=#6!9rfk00_)reor6h3y*3b%&pvok('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = False
+DEBUG = True
+#DEBUG = False
 ALLOWED_HOSTS = ['69.194.8.12','www.duanguiyuan.club',]
 
 # Application definition
