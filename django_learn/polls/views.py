d1f5f0917d1ab862f74bc48ac90d6d0257439f24
duanguiyuan <1092765650@qq.com>
2018-12-21 04:55:52 -0500

    添加使用通用视图

--- django_learn/polls/views.py
+++ django_learn/polls/views.py
@@ -1,13 +1,37 @@
-from django.shortcuts import render,get_object_or_404
+#使用通用视图
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from .models import Choice, Question
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+#关闭 Old
+
 
 # Create your views here.
-from django.http import HttpResponse,HttpResponseRedirect
-from django.template import loader
-from django.urls import reverse
-
-from .models import Question,Choice
 
 # 定义视图 显示的web页面
+'''
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
@@ -15,7 +39,6 @@ def index(request):
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context, request))
-#    return HttpResponse("Hello, world. You're at the polls index.")
 
 def indexindex(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -27,12 +50,11 @@ def indexindex(request):
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
-#    return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
-
+'''
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
