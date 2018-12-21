ce3cfc5566632a1a75127ee3d5b92fca32cef77f
duanguiyuan <1092765650@qq.com>
2018-12-21 04:09:16 -0500

    Choice 对象需要被管理

--- django_learn/polls/admin.py
+++ django_learn/polls/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 from .models import Question
+from .models import Choice
+
 # 问题 Question 对象需要被管理
 admin.site.register(Question)
+admin.site.register(Choice)
 
 # Register your models here.
