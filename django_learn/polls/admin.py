a205e9ddedf8a5d0b4f7e9c26ddc362a76539889
duanguiyuan <1092765650@qq.com>
2018-12-24 01:59:07 -0500

    表单排序

--- django_learn/polls/admin.py
+++ django_learn/polls/admin.py
@@ -3,8 +3,13 @@ from django.contrib import admin
 from .models import Question
 from .models import Choice
 
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
 # 问题 Question 对象需要被管理
-admin.site.register(Question)
+#admin.site.register(Question)
+
+admin.site.register(Question,QuestionAdmin)
 admin.site.register(Choice)
 
 # Register your models here.
