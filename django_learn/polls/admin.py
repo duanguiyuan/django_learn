46b440f5ba7bd37bfeadcf05ba070773103e9078
duanguiyuan <1092765650@qq.com>
2018-12-25 04:24:51 -0500

    页面插入

--- django_learn/polls/admin.py
+++ django_learn/polls/admin.py
@@ -3,9 +3,12 @@ from django.contrib import admin
 from .models import Question
 from .models import Choice
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [('TTT',{'fields':['question_text']}),('Date information', {'fields':['pub_date']}),]
-
+    inlines = [ChoiceInline]
 # 问题 Question 对象需要被管理
 #admin.site.register(Question)
 
